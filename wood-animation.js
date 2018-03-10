5ace6a18b975ee3147cd7be05ceb7fcdfa897abc
chroniczny <chroniczny@gmail.com>
2018-03-10 13:48:25 +0100

    fixed img height; animation on it when hover

--- wood-animation.js
+++ wood-animation.js
@@ -2,20 +2,23 @@
 $(document).ready(function () {
     var smallMediaWidth = 768;
     var viewportWidth = $(window).outerWidth();
-    var firstStripes = $('.stripes-first').find('.stripe');
+    var firstStripes = $('.wood-animated');
     $(firstStripes).each(function (i) {
         var top_of_object = $(this).offset().top;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
         var top_of_window = $(window).scrollTop();
+        var bottom_of_object =
+            $(this).offset().top + $(this).outerHeight();
 
-        if (top_of_object > top_of_window) {
-            $(this).addClass('visible');
+        if (bottom_of_object > top_of_window && top_of_object < bottom_of_window ) {
+            $(this).addClass('animate');
         }
     });
 
     /* Every time the window is scrolled ... */
     $(window).scroll(function () {
         /* Check the location of each desired element */
-        $('.stripe').each(function (i) {
+        $('.wood-animated').each(function (i) {
             var bottom_of_object =
                 $(this).offset().top + $(this).outerHeight();
             var top_of_object = $(this).offset().top;
@@ -23,18 +26,13 @@ $(document).ready(function () {
                 $(this).offset().top + $(this).outerHeight() / 2;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             var top_of_window = $(window).scrollTop();
-            /* If the object is completely visible in the window, fade it it */
+            /* If the object is completely visible in the window, fade in it */
 
-            if (viewportWidth < smallMediaWidth) {
-                if (top_of_object < bottom_of_window) {
-                    $(this).addClass('visible');
+                if (bottom_of_object > top_of_window && top_of_object < bottom_of_window ) {
+                    $(this).addClass('animate');
+                } else {
+                    $(this).removeClass('animate');
                 }
-            }
-            else {
-                if (bottom_of_window > bottom_of_object) {
-                    $(this).addClass('visible');
-                }
-            }
         });
     });
 
