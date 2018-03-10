ba0ee1e3fcc1b2ea8dd08812e1d7a46c3d68dcdb
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-03-10 13:56:41 +0100

    tiding

--- wood-animation.js
+++ wood-animation.js
@@ -1,9 +1,8 @@
 
 $(document).ready(function () {
     var smallMediaWidth = 768;
-    var viewportWidth = $(window).outerWidth();
-    var firstStripes = $('.wood-animated');
-    $(firstStripes).each(function (i) {
+    var woodWave = $('.wood-animated');
+    $(woodWave).each(function (i) {
         var top_of_object = $(this).offset().top;
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         var top_of_window = $(window).scrollTop();
@@ -15,18 +14,13 @@ $(document).ready(function () {
         }
     });
 
-    /* Every time the window is scrolled ... */
     $(window).scroll(function () {
-        /* Check the location of each desired element */
         $('.wood-animated').each(function (i) {
             var bottom_of_object =
                 $(this).offset().top + $(this).outerHeight();
             var top_of_object = $(this).offset().top;
-            var middle_of_object =
-                $(this).offset().top + $(this).outerHeight() / 2;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             var top_of_window = $(window).scrollTop();
-            /* If the object is completely visible in the window, fade in it */
 
                 if (bottom_of_object > top_of_window && top_of_object < bottom_of_window ) {
                     $(this).addClass('animate');
