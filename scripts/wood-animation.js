fa974ae5d53d711f9eddbaed49b7e1728f1b864d
chroniczny <chroniczny@gmail.com>
2018-03-11 15:10:35 +0100

    solving conflicts continued

--- scripts/wood-animation.js
+++ scripts/wood-animation.js
@@ -2,37 +2,31 @@
 $(document).ready(function () {
 
     var potContainer = $('.pot-container');
-    var pot = $('.pot');
+    var pot = potContainer.find('.icon-box');
 
     pot.load('../img/pot_icon1.svg', function () {
         pot.parent().find('img').css('display', 'none');
     });
 
     var woodWave = $('.wood-animated');
-    woodWave.each(function (i) {
-        var top_of_object = $(this).offset().top;
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        var top_of_window = $(window).scrollTop();
-        var bottom_of_object =
-            $(this).offset().top + $(this).outerHeight();
 
-        if (bottom_of_object > top_of_window && top_of_object < bottom_of_window ) {
-            $(this).addClass('animate');
-        }
-    });
+    $(window).scroll(function () {
+        var marginPoint = 100;
 
-    woodWave.scroll(function () {
-        $('.wood-animated').each(function (i) {
-            var bottom_of_object =
-                $(this).offset().top + $(this).outerHeight();
+        woodWave.each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var top_of_object = $(this).offset().top;
+            var middle_top_of_window = $(window).scrollTop() + marginPoint;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var middle_bottom_of_window = bottom_of_window - marginPoint;
             var top_of_window = $(window).scrollTop();
 
-                if (bottom_of_object > top_of_window && top_of_object < bottom_of_window ) {
+                if (bottom_of_object > middle_top_of_window && top_of_object < middle_bottom_of_window ) {
+                    console.log('set animation');
                     $(this).addClass('animate');
                 } else {
                     $(this).removeClass('animate');
+                    console.log('no no');
                 }
         });
     });
